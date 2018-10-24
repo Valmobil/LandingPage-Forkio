3e931a089838b50fa5d527e9f300d62cc2220c66
val <valmobil@gmail.com>
2018-10-24 20:50:04 +0300

    Gulp Script update v2

--- gulpfile.js
+++ gulpfile.js
@@ -1,87 +1,98 @@
-let gulp = require('gulp');
+let gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
-    concatCss = require('gulp-concat-css'),
     cleanCSS = require('gulp-clean-css'),
     pump = require('pump'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    clean = require('gulp-clean');
 
-
-// tasks for HTML
-gulp.task('copy-html', () => {
+gulp.task('copy-html',()=>{
     return gulp.src('./src/**/*.html')
         .pipe(gulp.dest('./dist/'));
 });
 
+gulp.task('copy-dist',()=>{
+    return gulp.src('./dist/, {read: false})
+        .pipe(clean());
+});
+
 // tasks for CSS files
 
-gulp.task('sass',()=> {
+gulp.task('clean-css', ()=> {
+    return gulp.src('./src/css/', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('sass',['clean-css'], ()=> {
     return gulp.src('./src/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./src/css/'));
 });
-//
-gulp.task('autoprefix', gulp.series('sass'),() =>
-    gulp.src('./src/css/**/*.css')
+
+gulp.task('autoprefix', ['sass'], ()=> {
+    return gulp.src('./src/css/**/*.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
         .pipe(gulp.dest('./src/css/'))
-);
+});
 
-gulp.task('concat-css', gulp.series('autoprefix'), ()=>{
+gulp.task('concat-css', ['autoprefix'], ()=>{
     return gulp.src('./src/css/**/*.css')
-        .pipe(concatCss('style.css'))
+        .pipe(concat('style.css'))
         .pipe(gulp.dest('./src/css/'));
 });
 
-gulp.task('minify-css', ()=>{
+gulp.task('minify-css', ['concat-css'], ()=>{
     return gulp.src('./src/css/style.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./src/css/'));
 });
 
-gulp.task('copy-css', gulp.series('concat-css'), () => {
+gulp.task('copy-css', ['minify-css'], () => {
     return gulp.src('./src/css/style.css')
         .pipe(gulp.dest('./dist/css/'))
 });
 
-//tasks for JS files
+// tasks for JS files
 
-gulp.task('concat-js', () => {
-    return gulp.src('./src/js/!**!/!*.js')
+gulp.task('clean-js', ()=> {
+    return gulp.src('./src/js/script.js', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('concat-js',['clean-js'],()=>{
+    return gulp.src('./src/js/**/*.js')
         .pipe(concat('script.js'))
         .pipe(gulp.dest('./src/js/'));
 });
 
+gulp.task('minify-js', ['concat-js'], (cb) => {
+    pump([
+            gulp.src('./src/js/script.js'),
+            uglify(),
+            gulp.dest('./src/js/')
+        ],
+        cb
+    );
+});
 
+gulp.task('copy-js',['minify-js'], () => {
+    return gulp.src('./src/js/script.js')
+        .pipe(gulp.dest('./dist/js/'))
+});
 
-// gulp.task('minify-js', ['concat-js'], (cb) => {
-//     pump([
-//             gulp.src('./src/js/script.js'),
-//             uglify(),
-//             gulp.dest('./src/js/')
-//         ],
-//         cb
-//     );
-// });
+gulp.task('serve', ['copy-html', 'copy-css', 'copy-js'], ()=> {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
 
-// gulp.task('copy-js', ['minify-js'], () => {
-//     return gulp.src('./src/js/script.js')
-//         .pipe(gulp.dest('./dist/js/'))
-// });
-
-// gulp.task('serve', ['copy-html', 'copy-css', 'copy-js'], () => {
-//     browserSync.init({
-//         server: {
-//             baseDir: "./dist"
-//         }
-//     });
-//
-//     gulp.watch('./src/!**!/!*.html', ['copy-html']).on('change', browserSync.reload);
-//     gulp.watch('./src/scss/!**!/!*.scss', ['copy-css']).on('change', browserSync.reload);
-//     gulp.watch('./src/js/!**!/!*.js', ['copy-js']).on('change', browserSync.reload);
-// });
+    gulp.watch('./src/**/*.html', ['copy-html']).on('change', browserSync.reload);
+    gulp.watch('./src/scss/**/*.scss', ['copy-css']).on('change', browserSync.reload);
+    gulp.watch('./src/js/**/*.js', ['copy-js']).on('change', browserSync.reload);
+});
