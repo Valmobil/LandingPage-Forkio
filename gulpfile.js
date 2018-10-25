dc7769701055648956e0747d201b35e66366721b
Oleg <tytarenkooleg@gmail.com>
2018-10-25 08:29:22 +0300

    commit5 (update html and gulpfile)

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,39 @@ let gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
     pump = require('pump'),
     uglify = require('gulp-uglify'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+    imagemin = require('gulp-imagemin');
 
-gulp.task('copy-html',()=>{
+gulp.task('copy-html', () => {
     return gulp.src('./src/**/*.html')
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('clear-dist',()=>{
+gulp.task('copy-plugins',()=>{
+    return gulp.src('./src/plugins/**/*')
+        .pipe(gulp.dest('./dist/plugins/'));
+});
+
+
+gulp.task('clean-dist', () => {
     return gulp.src('./dist/', {read: false})
         .pipe(clean());
 });
 
 // tasks for CSS files
 
-gulp.task('clean-css', ()=> {
+gulp.task('clean-css', () => {
     return gulp.src('./src/css/', {read: false})
         .pipe(clean());
 });
 
-gulp.task('sass',['clean-css'], ()=> {
+gulp.task('sass', ['clean-css'], () => {
     return gulp.src('./src/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('./src/css/'));
 });
 
-gulp.task('autoprefix', ['sass'], ()=> {
+gulp.task('autoprefix', ['sass'], () => {
     return gulp.src('./src/css/**/*.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
@@ -40,13 +47,13 @@ gulp.task('autoprefix', ['sass'], ()=> {
         .pipe(gulp.dest('./src/css/'))
 });
 
-gulp.task('concat-css', ['autoprefix'], ()=>{
+gulp.task('concat-css', ['autoprefix'], () => {
     return gulp.src('./src/css/**/*.css')
         .pipe(concat('style.css'))
         .pipe(gulp.dest('./src/css/'));
 });
 
-gulp.task('minify-css', ['concat-css'], ()=>{
+gulp.task('minify-css', ['concat-css'], () => {
     return gulp.src('./src/css/style.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./src/css/'));
@@ -59,12 +66,12 @@ gulp.task('copy-css', ['minify-css'], () => {
 
 // tasks for JS files
 
-gulp.task('clean-js', ()=> {
+gulp.task('clean-js', () => {
     return gulp.src('./src/js/script.js', {read: false})
         .pipe(clean());
 });
 
-gulp.task('concat-js',['clean-js'],()=>{
+gulp.task('concat-js', ['clean-js'], () => {
     return gulp.src('./src/js/**/*.js')
         .pipe(concat('script.js'))
         .pipe(gulp.dest('./src/js/'));
@@ -80,12 +87,21 @@ gulp.task('minify-js', ['concat-js'], (cb) => {
     );
 });
 
-gulp.task('copy-js',['minify-js'], () => {
+gulp.task('copy-js', ['minify-js'], () => {
     return gulp.src('./src/js/script.js')
         .pipe(gulp.dest('./dist/js/'))
 });
 
-gulp.task('serve', ['copy-html', 'copy-css', 'copy-js'], ()=> {
+// tasks for images
+
+gulp.task('minify-img',()=>{
+    return gulp.src('./src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img/'))
+})
+
+
+gulp.task('serve', ['copy-html','copy-css','copy-js','minify-img'], () => {
     browserSync.init({
         server: {
             baseDir: "./dist"
@@ -95,4 +111,15 @@ gulp.task('serve', ['copy-html', 'copy-css', 'copy-js'], ()=> {
     gulp.watch('./src/**/*.html', ['copy-html']).on('change', browserSync.reload);
     gulp.watch('./src/scss/**/*.scss', ['copy-css']).on('change', browserSync.reload);
     gulp.watch('./src/js/**/*.js', ['copy-js']).on('change', browserSync.reload);
+});
+
+gulp.task('dev',['clean-dist'],()=>{
+    gulp.start('serve');
+});
+
+gulp.task('build', ['clean-dist'],()=>{
+    gulp.start('copy-html');
+    gulp.start('copy-css');
+    gulp.start('copy-js');
+    gulp.start('minify-img');
 });
