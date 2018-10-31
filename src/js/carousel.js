12bb04161967f9c9a46b822ed0e50a7f4763d6b8
val <valmobil@gmail.com>
2018-10-31 19:37:12 +0200

    Carousel Section - 1200 px v2

---
+++ src/js/carousel.js
@@ -0,0 +1,14 @@
+console.log("I am here")
+$(document).ready(function(){
+    console.log("in carousel")
+    $('.carousel__list').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        arrows: true,
+        nextArrow: '<i class="fa fa-angle-right"></i>',
+        prevArrow: '<i class="fa fa-angle-left"></i>'
+});
+});
